758b8c33c9577635e42f3d4ab8bc8c4166274593
KsquaredK <ksquaredkelly@gmail.com>
2021-07-08 15:28:17 -0400

    add home,product components

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Home = () => (
+    <>
+        <h2>Kandy Korner</h2>
+        <h3>sweeeeEEEETTTT!!!</h3>
+
+        <address>
+            <div>Visit Us at the Nashville North Location</div>
+            <div>500 Puppy Way</div>
+        </address>
+    </>
+)
